c60b05c6642c98e14acccfcc8bf09c4d220239ce
DahaKot <32592512+DahaKot@users.noreply.github.com>
2017-10-07 21:05:28 +0300

    Working code

--- main.c
+++ main.c
@@ -1,37 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <mem.h>
 #include <assert.h>
 
-char *File_read(); //put the text prom file to char's array
+struct String {
+    unsigned long int len;
+    char *begin;
+};
+
+char *File_read(int *); //put the text prom file to char's array
 int N_str(const char * poem); //count lines in poem
 char **P_partition(char * poem, int); //creating char*'s array of pointers on the strings from poem
-void Sort(char ** , int);
+void Sort(struct String *, int);
 //int Comparator_of_strings(const char* first, const char* second); //meaning there are not 2 alike strings
-void File_write(char *); //writing the result in another file
+void File_write(struct String *, int); //writing the result in another file
 
 int main() {
-    char *poem = File_read();
+    int num_symbs = -1;
+    char *poem = File_read(&num_symbs);
     int num_strings = N_str(poem);
     char **s_pointers = P_partition(poem, num_strings);
 
+    struct String *strokes = (struct String *) calloc(num_strings, sizeof(struct String));
+    int i = 0;
+    for ( ; i < num_strings - 1; i++) {
+        strokes[i].begin = s_pointers[i];
+        strokes[i].len = s_pointers[i+1] - s_pointers[i];
+    }
+    strokes[i].begin = s_pointers[i];
+    strokes[i].len = num_symbs - (s_pointers[i] - s_pointers[0]);
     //Here the trouble begin
-    Sort(s_pointers, num_strings);
+    Sort(strokes, num_strings);
+
+    File_write(strokes, num_strings);
 
     free(poem);
     free(s_pointers);
+    free(strokes);
 
     return 0;
 }
 
-char *File_read() {
+char *File_read(int *n) {
     FILE *f_poem;
-    f_poem = fopen("Onegin.txt", "r");
+    f_poem = fopen("out.txt", "r");
     assert(f_poem != NULL); //with if?
 
     //finding numbers of characters in the f_poem
     fseek(f_poem, 0, SEEK_END);
     long int num_of_symb = ftell(f_poem);
+    *n = num_of_symb;
 
     //making char's array with text
     char *c_poem = calloc(num_of_symb, sizeof(char));
@@ -75,31 +92,25 @@ char ** P_partition(char *poem, int num_strings) {
     return s_pointers;
 }
 
-void Sort (char **pointers, int len) {
-    char **result = calloc(len, sizeof(char *));
-    int i = 0, j = 1;
+void Sort (struct String *str, int len) {
+    int i = 0, j = 0;
     for ( ; i < len; i++) {
-        for ( ; j < len; j++) {
+        for (j = i; j < len; j++) {
             int l = 0, k = 0;
-            while ((l < strlen(pointers[i])) && (l < strlen(pointers[j]))) {
-                if (*(pointers[i]) != *(pointers[j])) {
-                    k = (int) (*(pointers[i])) - (int) (*(pointers[j]));
+            while (l < str[i].len && l < str[j].len) {
+                if (str[i].begin[l] != str[j].begin[l]) {
+                    k = (int) (str[i].begin[l]) - (int) (str[j].begin[l]);
                     break;
                 }
                 l++;
             }
 
             if (k > 0) {
-                result[i] = pointers[j];
-            }
-            else {
-                result[i] = pointers[i];
+                struct String temp = str[j];
+                str[j] = str[i];
+                str[i] = temp;
             }
         }
-    }
-
-    for (i = 0; i < len; i++) {
-        File_write(result[i]);
     }
 }
 
@@ -113,14 +124,21 @@ void Sort (char **pointers, int len) {
     }
 }*/
 
-void File_write(char * poem) {
+void File_write(struct String *poem, int num_strings) {
     FILE *eginno;
-    eginno = fopen("Sorted.txt", "a");
+    eginno = fopen("Sorted.txt", "w");
     assert(eginno != NULL);
 
     int i = 0;
-    while (poem[i] != '\n' && poem[i] != EOF) {
-        fprintf(eginno, "%c", poem[i]);
+    while (i < num_strings) {
+        int j = 0;
+        while (j < poem[i].len) {
+            if (j == poem[i].len - 1 && poem[i].begin[j] != '\n') {
+                poem[i].begin[j] = '\n';
+            }
+            fprintf(eginno, "%c", poem[i].begin[j]);
+            j++;
+        }
         i++;
     }
 
